83205ce82ee9f134d0347be96573333ae54a83d3
Rubby <rubenslombard@pursuit.org>
2024-03-03 09:11:24 -0500

    adding new medical cabinet created

--- controllers/medicalController.js
+++ controllers/medicalController.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const medical = express.Router({ mergeParams: true });
-const { getMedicals } = require("../queries/medical");
+const { getMedicals, createMedical } = require("../queries/medical");
+const { getUser } = require("../queries/users");
 
 medical.get("/", async (req, res) => {
   const { user_id } = req.params;
   try {
-    const userMedical = await getMedicals(user_id);
-    res.status(200).json(userMedical);
+      const userMedical = await getMedicals(user_id);
+      const users = await getUser(user_id)
+    res.status(200).json({...users, userMedical});
   } catch (err) {
     res.status(404).json({ error: err });
   }
 });
+
+medical.post("/", async (req, res) => {
+    const { user_id } = req.params
+    const medical = await createMedical({ user_id, ...req.body })
+    res.status(200).json(medical)
+})
 
 
 module.exports = medical
--- queries/medical.js
+++ queries/medical.js
@@ -11,12 +11,19 @@ const getMedicals = async (user_id) => {
     }
 }
 
-const createMedical = async (medical) => {
+const createMedical = async (req, res) => {
     try {
         const { medical_history, blood_type, allergies, medication } = medical
-        const newMedical = await db.one("INSERT INTO medical (medical_history, blood_type, allergies, medication) VALUES ($1, $2, $3, $4) RETURNIN *",
-          [medical_history, blood_type, allergies, medication]
-        )
+        const newMedical = await db.one(
+          "INSERT INTO medical (medical_history, blood_type, allergies, medication, user_id) VALUES ($1, $2, $3, $4, $5) RETURNIN *",
+          [
+            medical.medical_history,
+            medical.blood_type,
+            medical.allergies,
+            medical.medication,
+            medical.user_id
+          ]
+        );
         return newMedical
    
     } catch (error) {
@@ -25,12 +32,15 @@ const createMedical = async (medical) => {
 
 }
 
-const updateMedical = async (medical_id, medical) => {
+const updateMedical = async (user_id, medical) => {
     try {
         const { medical_history, blood_type, allergies, medication } = medical
-        const updatedMedical = await db.one(
-          "UPDATE medical SET medical_history=$1, blood_type=$2, allergies=$3, medication=$4 WHERE medical_id=$5 RETURNING * ",
-          [medical_history, blood_type, allergies, medication]
+        const updatedMedical = await db.one("UPDATE medical SET medical_history=$1, blood_type=$2, allergies=$3, medication=$4 WHERE user_id=$5 RETURNING * ",
+            [medical_history,
+            blood_type,
+            allergies,
+            medication,
+            user_id]
         )
         return updatedMedical
         
@@ -39,7 +49,16 @@ const updateMedical = async (medical_id, medical) => {
     }
 }
 
+const deleteMedical = async (user_id) => {
+    try {
+        const deletedMedical = await db.one("DELETE FROM medical WHERE user_id=$1 RETURNING *", user_id)
+        return deletedMedical
+    } catch (error) {
+        return error
+    }
+}
 
 
 
-module.exports  = { getMedicals, createMedical, updateMedical}
+
+module.exports  = { getMedicals, createMedical, updateMedical, deleteMedical}
